9f71611e8b2a02734e71c4bcff0f5ca2053cc242
prumin/Yamamoto <puru42745@gmail.com>
2024-10-09 04:20:27 +0900

    完了前

---
+++ EnemyDownApp/src/main/java/plugin/enemydown/app/mapper/GameConfigMapper.java
@@ -0,0 +1,20 @@
+package plugin.enemydown.app.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import java.util.List;
+import plugin.enemydown.app.mapper.data.GameConfig;
+import plugin.enemydown.app.mapper.data.SpawnEnemy;
+
+@Mapper
+public interface GameConfigMapper {
+
+  @Select("select * from game_config order by id asc")
+  List<GameConfig> selectConfigList();
+
+  @Select("select * from game_config where difficulty = #{difficulty} order by id asc")
+  GameConfig selectConfig(String difficulty);
+
+  @Select("select * from spawn_enemy inner join game_config on spawn_enemy.difficulty = game_config.difficulty where spawn_enemy.difficulty = #{difficulty} order by spawn_enemy.id asc")
+  List<SpawnEnemy> selectSpawnEnemyList(String difficulty);
+}
---
+++ EnemyDownApp/src/main/java/plugin/enemydown/app/service/ConfigService.java
@@ -0,0 +1,29 @@
+package plugin.enemydown.app.service;
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import plugin.enemydown.app.mapper.GameConfigMapper;
+import plugin.enemydown.app.mapper.data.GameConfig;
+import plugin.enemydown.app.mapper.data.SpawnEnemy;
+
+@Service
+public class ConfigService {
+
+  @Autowired
+  private GameConfigMapper mapper;
+
+  public List<GameConfig> searchConfigList() {
+    return mapper.selectConfigList();
+  }
+
+  public GameConfig searchConfig(String difficulty) {
+    return mapper.selectConfig(difficulty);
+  }
+
+  public List<SpawnEnemy> searchSpawnEnemyList(String difficulty) {
+    return mapper.selectSpawnEnemyList(difficulty);
+  }
+}
+
